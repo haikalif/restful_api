dd331d0e5dc9583899837719b545d0ab1b323d12
Haikal <haikalisratulfajri@gmail.com>
2026-07-12 10:40:46 +0700

    fitur sactum (menambah penjaga akses)

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Guard;
 
-
-Route::get('/todos', [App\Http\Controllers\TodoController::class, 'index']);
-Route::Post('/todos', [App\Http\Controllers\TodoController::class, 'store']);
-route::get('/todos/{todo}', [App\Http\Controllers\TodoController::class, 'show']);
-Route::put('/todos/{todo}', [App\Http\Controllers\TodoController::class, 'update']);
-route::delete('/todos/{todo}', [App\Http\Controllers\TodoController::class, 'destroy']);
+route::middleware('auth:sanctum')->group(function () {
+    Route::get('/todos', [App\Http\Controllers\TodoController::class, 'index']);
+    Route::Post('/todos', [App\Http\Controllers\TodoController::class, 'store']);
+    route::get('/todos/{todo}', [App\Http\Controllers\TodoController::class, 'show']);
+    Route::put('/todos/{todo}', [App\Http\Controllers\TodoController::class, 'update']);
+    route::delete('/todos/{todo}', [App\Http\Controllers\TodoController::class, 'destroy']);
+});
