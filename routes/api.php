2ff500926d915c576e2815663d91a8beff953bb0
Haikal <haikalisratulfajri@gmail.com>
2026-07-14 10:58:14 +0700

    route log out

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ route::middleware('auth:sanctum')->group(function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+route::post('/logout', [AuthController::class, 'logout' ]);
